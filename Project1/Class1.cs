8ca4971dfba4c717343ac2da0b96a222ff18fff7
TrsDat <tranhoangdat04072007@gmail.com>
2026-08-22 00:28:28 +0700

    Add project files.

---
+++ Project1/Class1.cs
@@ -0,0 +1,10 @@
+﻿namespace Project1
+{
+    public class class1
+    {
+        public static void InLoiChao()
+        {
+            Console.WriteLine("Hello cs!");
+        }
+    }
+}
---
+++ Project1/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Project1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Gọi phương thức từ file FileName.cs
+            class1.InLoiChao();
+        }
+    }
+}
